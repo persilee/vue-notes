7d716ed1277c179739a767435d06c8b90be316dd
persilee <i@lishaoy.net>
2018-11-20 10:41:13 +0800

    🍊 add note and show

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,26 @@
+const Editor = {
+    props: [
+        'entityObject'
+    ],
+    data(){
+        return {
+            entity: this.entityObject
+        }
+    },
+    template: `
+        <div class="ui form">
+            <div class="field">
+                <textarea 
+                    cols="30" 
+                    rows="5" 
+                    placeholder="写点啥..."
+                    v-model="entity.body"
+                ></textarea>
+            </div>
+        </div>
+    `
+}
+
 const Note = {
     props: [
         'entityObject'
@@ -7,11 +30,20 @@ const Note = {
             entity: this.entityObject
         }
     },
+    components: {
+        Editor,
+    },
     template: `
         <div class="item">
             <div class="content">
                 <div class="header">
-                    {{ entity.body }}
+                    {{ entity.body || '新建笔记' }}
+                </div>
+                <div class="extra">
+                    <editor 
+                        :entity-object="entity"
+                    >
+                    </editor>
                 </div>
             </div>
         </div>
@@ -32,11 +64,23 @@ const Notes = {
             .then(collection => {
                 const _entities = collection.chain()
                     .find()
-                    .simplesort('$loki', 'isdesc')
+                    .simplesort('$loki', true)
                     .data();
                 this.entities = _entities;
                 console.log(this.entities);
             })
+    },
+    methods: {
+        create(){
+            loadCollection('notes')
+                .then((collection) => {
+                   const entity = collection.insert({
+                        body: ''
+                    })
+                    db.saveDatabase()
+                    this.entities.unshift(entity)
+                })
+        }
     },
     template: `
         <div class="ui container notes">
@@ -44,11 +88,14 @@ const Notes = {
                 <i class="bookmark outline icon"></i>
                 Notes App with Vue.js
             </h4>
-            <a class="ui right floated basic violet button">添加笔记</a>
+            <a 
+                class="ui right floated basic violet button"
+                @click="create"
+            >添加笔记</a>
             <div class="ui divided items">
                 <note 
                 v-for="entity in entities"
-                :entityObject="entity"
+                :entity-object="entity"
                 :key="entity.$loki"
                 ></note>
             </div>
